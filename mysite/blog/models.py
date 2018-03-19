e4bf7ce492b28df51382077b899b74b7dc42b769
AwesomeHaircut <jesusbalbastro@gmail.com>
2018-03-19 14:09:59 -0700

    started working on the blog models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BlogPost(models.Model):
+    """
+    Hopefully this class is all I need for a blog post.
+    """
+    title = models.CharField(max_length=70)
+    date = models.DateField()
+    content = models.TextField()
+    ispublished = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    """
+    hmm what
+    """
+    pass
