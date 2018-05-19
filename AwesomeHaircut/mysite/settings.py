f4c2c90dbe6efcef68851e0329ab5acba8779878
AwesomeHaircut <jesusbalbastro@gmail.com>
2018-05-19 08:25:28 -0700

    Modified permissions and some beepbops

--- AwesomeHaircut/mysite/settings.py
+++ AwesomeHaircut/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=zt^*$1b3fn#6hescc%se91z03a+6np0m^7cn%0k=7^a%(kpy9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.awesomehaircut.xyz']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 **A small blog made in Django   **
+if you know what I mean
+
