7d031dd41a5c7063a063093a8ab71a06b7712386
AwesomeHaircut <jesusbalbastro@gmail.com>
2018-05-20 06:24:10 -0700

    Admin should be under blog

--- AwesomeHaircut/blog/urls.py
+++ AwesomeHaircut/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
     path('<str:post_title>', views.blog_post, name='title'),
 ]
 
--- AwesomeHaircut/mysite/urls.py
+++ AwesomeHaircut/mysite/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
 
 from . import views
@@ -21,7 +20,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),
     path('about/', views.about),
     path('<str:notfound>', views.notfound, name='notfound'),
 ]
